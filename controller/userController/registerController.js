f9b009bb4084ed09e55125ed0ce29c599ec14b4f
Rejoice <uahomorejoice@gmail.com>
2022-06-18 03:25:20 +0100

    fix login route

--- controller/userController/registerController.js
+++ controller/userController/registerController.js
@@ -10,13 +10,13 @@ const registerController = async (req, res) => {
   let check = await User.findOne({ username: req.body.username });
   if (!check) {
     let user = await User.create(req.body);
-    if (user.role === "0") user.role = "Employee";
-    if (user.role === "1") user.role = "Customer";
+    const { password, ...mainUser } = user._doc;
+    if (mainUser.role === "0") mainUser.role = "Employee";
+    if (mainUser.role === "1") mainUser.role = "Customer";
     //signing jwt
     // const token = jwt.sign({ id: user._id }, process.env.TOKEN_KEY, {
     //   expiresIn: "90d",
     // });
-    const { password, ...mainUser } = user._doc
     res.status(200).json(mainUser);
   } else {
     res.status(201).json({ message: "already registered" });
