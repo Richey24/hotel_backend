4782bc540c75b7ce659966274a32d7bf159de466
Rejoice <uahomorejoice@gmail.com>
2022-07-07 01:43:18 +0100

    get single user

--- controller/userController/getCustomer.js
+++ controller/userController/getCustomer.js
@@ -1,12 +1,15 @@
-const { User } = require('../../schema')
+const { User } = require("../../schema");
 
 const getCustomer = async (req, res) => {
-    try {
-        const user = await User.findOne({ _id: req.params.id })
-        res.status(200).json(user)
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    const user = await User.findOne({ _id: req.params.id });
+    const { password, ...mainUser } = user._doc;
+    if (mainUser.role === "0") mainUser.role = "Employee";
+    if (mainUser.role === "1") mainUser.role = "Customer";
+    res.status(200).json(mainUser);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-module.exports = getCustomer
+module.exports = getCustomer;
