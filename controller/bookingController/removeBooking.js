4b70197dfddaf9257d1623bb8fab04058f802fb2
Rejoice <uahomorejoice@gmail.com>
2022-07-26 03:44:47 +0100

    changes

--- controller/bookingController/removeBooking.js
+++ controller/bookingController/removeBooking.js
@@ -1,13 +1,18 @@
-const { Room, Booking } = require('../../schema')
+const { Room, Booking } = require("../../schema");
 
 const removeBookingController = async (req, res) => {
-    if (!req.params.id) {
-        res.status(203).json({ message: "send the id of the booking you wish to remove in the request parameter" })
-    } else {
-        const book = await Booking.findById(req.params.id)
-        const room = await Room.findOneAndUpdate({ roomNum: book.roomNum }, { isAvailable: true, bkId: "" }, { new: true })
-        res.status(200).json(room)
-    }
-}
+  if (!req.params.id) {
+    res
+      .status(203)
+      .json({
+        message:
+          "send the id of the booking you wish to remove in the request parameter",
+      });
+  } else {
+    const book = await Booking.findByIdAndDelete(req.params.id);
+    // const room = await Room.findOneAndUpdate({ roomNum: book.roomNum }, { isAvailable: true, bkId: "" }, { new: true })
+    res.status(200).json(book);
+  }
+};
 
-module.exports = removeBookingController
+module.exports = removeBookingController;
